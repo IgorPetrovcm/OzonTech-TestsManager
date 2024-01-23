d129bb7b17ab6cdd980707641a3a9299f25bf4f4
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-23 20:48:08 +0300

    Основной прототип

---
+++ TestsManager/TestTasksManager/DataTasks.cs
@@ -0,0 +1,20 @@
+﻿namespace TestTasksManager
+{
+	public class DataTasks
+	{
+		private int _count;
+
+		private IEnumerable<string>? _lines;
+
+		public int Count { get { return _count; } }
+
+		public IEnumerable<string>? Lines { get { return _lines; } }
+
+		public DataTasks(int count,  IEnumerable<string> lines)
+		{
+			_lines = lines;
+
+			_count = count;
+		}
+	}
+}
---
+++ TestsManager/TestTasksManager/OzonData.cs
@@ -0,0 +1,17 @@
+﻿namespace TestTasksManager;
+
+using System.IO;
+
+public static class OzonData
+{ 
+	public static DataTasks UploadTask(string[] lines)
+	{
+		string[] taskLines = new string[lines.Length - 1];
+
+		Array.Copy(lines, 1, taskLines, 0, taskLines.Length);
+
+		return new DataTasks(int.Parse(lines[0]), taskLines);
+	}
+
+	public static 
+}
