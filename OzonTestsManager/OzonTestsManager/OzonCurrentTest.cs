142af29d961146c9e98e6b8c5ceeb6749bf66a3e
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-28 13:35:17 +0300

    В OzonCurrentTest добавлены удобные свойства для более быстрого в коде доступа к данным

--- OzonTestsManager/OzonTestsManager/OzonCurrentTest.cs
+++ OzonTestsManager/OzonTestsManager/OzonCurrentTest.cs
@@ -13,6 +13,17 @@ public class OzonCurrentTest
 
 	public IEnumerable<DataTaskResult>? Result {get {return _result;} }
 
+	public int TaskCount {get {
+		if (_task == null)
+			return 0;
+		else 
+			return _task.count;
+	}}
+
+	public string[] ArrayTasks {get {
+		return _task.lines.ToArray();
+	}}
+
 
 	public OzonCurrentTest()
 	{
@@ -37,31 +48,10 @@ public class OzonCurrentTest
 	{
 		_task = task;
 	}
-	public void UploadTask(string[] lines)
-	{
-		string[] tasks = new string[lines.Length - 1];
-
-		Array.Copy(lines, 1, tasks, 0, lines.Length);
-
-		_task = new DataTask(int.Parse(lines[0]), tasks);
-	}
 
 
 	public void UploadTaskResult(IList<DataTaskResult> result)
 	{
 		_result = result;
-	}
-	public void UploadTaskResult(string[] lines)
-	{
-		_result = new List<DataTaskResult>() ?? null;
-
-		List<DataTaskResult> newResult = new List<DataTaskResult>();
-
-		for (int i = 0; i < lines.Length; i++)
-		{
-			newResult.Add(new DataTaskResult(i + 1, lines[i]));
-		}
-
-		_result = newResult;
 	}
 }
