1b108dce51189386696b84d8856279262c21ec4c
Игорь Петров <igorpetrov42ru@gmail.com>
2024-02-05 23:57:04 +0300

    Создал прототип OzonTestsDelivery

---
+++ OzonTestsManager/OzonAllManager.cs
@@ -0,0 +1,8 @@
+namespace OzonTestsManager;
+
+
+
+public class OzonAllManager
+{
+    
+}
---
+++ OzonTestsManager/TestDelivery/OzonTestsDelivery.cs
@@ -0,0 +1,93 @@
+namespace OzonTestsManager;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using HtmlAgilityPack;
+
+public class OzonTestsDelivery
+{
+    private readonly Uri? _uriTask;
+
+    public Uri? UriTask {get {return _uriTask;}}
+
+
+    public OzonTestsDelivery() {}
+
+    public OzonTestsDelivery(Uri uriTask)
+    {
+        _uriTask = uriTask;
+    }
+
+    public OzonTestsDelivery(string strUriTask)
+    {
+        _uriTask = new Uri(strUriTask);
+    }
+
+
+    public HttpContent GetTestArchive()
+    {
+        if (_uriTask is null)
+            return null;
+
+        string  downloadUri = DeliveryParser.GetHrefToArchiveFile(_uriTask);
+
+        HttpResponseMessage response = DeliveryClient.Send(HttpMethod.Get,downloadUri);
+
+        
+    }
+
+    private static class DeliveryClient
+    {
+        public static HttpClient? httpClient;
+
+        public static HttpRequestMessage? request;
+
+        public static HttpResponseMessage? response;
+        
+        public static async Task<HttpResponseMessage> SendAsync(HttpMethod method, Uri uri)
+        {
+            httpClient = new HttpClient();
+
+            request = new HttpRequestMessage(method, uri);
+
+            return await httpClient.SendAsync(request);
+        }
+
+        public static HttpResponseMessage Send(HttpMethod method, Uri uri)
+        {
+            httpClient = new HttpClient();
+
+            request = new HttpRequestMessage(method, uri);
+
+            return httpClient.SendAsync(request).Result;
+        }
+        public static HttpResponseMessage Send(HttpMethod method, string uri)
+        {
+            httpClient = new HttpClient();
+
+            request = new HttpRequestMessage(method, uri);
+
+            return httpClient.SendAsync(request).Result;
+        }
+    }
+
+    private static class DeliveryParser
+    {
+        private static HtmlDocument? document;
+
+        private static HtmlWeb? webFromDocument;
+
+        public static readonly string xPath = @"//*[@id=""__nuxt""]/div/main/div/section/div[2]/div[1]/div[2]/div/div[2]/div/a";
+
+
+        public static string GetHrefToArchiveFile(Uri uri)
+        {
+            webFromDocument = new HtmlWeb();
+            document = webFromDocument.Load(uri);
+
+            HtmlNode href = document.DocumentNode.SelectSingleNode(xPath);
+
+            return href.Attributes["href"].Value;
+        }
+    }
+}
